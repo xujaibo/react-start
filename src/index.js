e2aa19f2bcaaea2671e1cf28d6f5a8478af01cb9
xujiabo <xujp@oceansoft.com.cn>
2020-08-18 23:19:05 +0800

    jsx初步提交

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let names = ['xujiabo', 'gaozehngyan']
+let name = 'xujiabo'
+let flag = true
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <p className='namecss'>i am {name}</p>
+        {flag ? <h1>hello React</h1> : <h1>goodBay React</h1>}
+        {
+            names.map((name, index) =>
+                <p key={index}>hello,i am {name}</p>
+            )
+        }
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
